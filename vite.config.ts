47e7a6f63e6cb5c4b9fecf277b5ba406ee9acfa6
Florian Beisel <florian@beisel.org>
2026-04-13 23:24:33 +0200

    Fix GitHub Pages base URL for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/beisel.works/" : "/",
+export default defineConfig(() => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
